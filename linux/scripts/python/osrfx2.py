a1bf0669e3e1047a6ccd72a58c0c07bb76f9df9f
Chen Wang <unicorn_wang@outlook.com>
2014-02-21 08:05:37 +0800

    Update osrfx2.py

--- linux/scripts/python/osrfx2.py
+++ linux/scripts/python/osrfx2.py
@@ -16,7 +16,6 @@ Options:
 '''
 __author__		= "unicon wang (unicorn_wang@outlook.com)"
 __version__		= "$Revision: 0.1 $"
-__date__		= "$Date: 2004/05/05 21:57:19 $"
 __copyright__	= "Copyright (c) 2014 Unicorn Wang"
 __license__		= "GPLv2"
 
@@ -28,7 +27,13 @@ import getopt
 from getch import getch
 
 __DEBUG = 1
-DEVICE_PATH = ('', '') # store device path in devFS and sysFS
+
+BARGRAPH_ON = 0x80
+BARGRAPH_OFF = 0x00
+
+BARGRAPH_MAXBAR = 4 #CY001 only have 4 LED bars availabe
+
+_device_path = ('', '') # store device path in (devFS, sysFS)
 
 _flag_read					= False
 _len_read					= 512
@@ -138,11 +143,74 @@ def get_mouse_position_by_interrupt():
 	'''
 	pass
 
-def get_bargraph_display():
+def set_bargraph_display(value):
+	'''Function to set bar-graph display.
+
+	'''
+	str_value = "%d" %(value)
+	command = 'echo' + ' ' + str_value + ' > ' + _device_path[1] + '/device/bargraph ' + '2>/dev/null'
+	#(status, output) = getstatusoutput(command)
+	#return status
+	return 0
+
+def handle_light_one_bar():
+	print("Which Bar (input number 1 thru %d)?" %(BARGRAPH_MAXBAR))
+
+	try:
+		bar = int(getch())
+	except:
+		print("Error reading input!")
+		return
+
+	if(bar == 0 or bar > BARGRAPH_MAXBAR):
+		print("Invalid bar number!")
+		return
+
+	bar = bar - 1 #normalize to 0 to 3
+
+	barValue = 1 << bar
+	barValue = barValue | BARGRAPH_ON
+
+	if( 0 != set_bargraph_display(barValue)):
+		print("set_bargraph_display failed with error")
+		return
+
+
+def handle_clear_one_bar():
+	print("Which Bar (input number 1 thru %d)?" %(BARGRAPH_MAXBAR))
+	try:
+		bar = int(getch())
+	except:
+		print("Error reading input!")
+		return
+
+	if(bar == 0 or bar > BARGRAPH_MAXBAR):
+		print("Invalid bar number!")
+		return
+
+	bar = bar - 1 #normalize to 0 to 3
+	barValue = BARGRAPH_OFF; # reset and flag off
+	barValue = barValue | (1 << bar)
+
+	if (0 != set_bargraph_display(barValue)):
+		print("set_bargraph_display failed with error")
+		return
+
+def handle_light_all_bars():
+	if( 0 != set_bargraph_display(0x8F)):
+		print("set_bargraph_display failed with error")
+		return
+
+def handle_clear_all_bars():
+	if( 0 != set_bargraph_display(0x0F)):
+		print("set_bargraph_display failed with error")
+		return
+
+def handle_get_bar_state():
 	'''Function to get bar-graph display status.
 
 	'''
-	#command = 'cat' + ' ' + DEVICE_PATH[1] + '/device/bargraph ' + '2>/dev/null'
+	#command = 'cat' + ' ' + _device_path[1] + '/device/bargraph ' + '2>/dev/null'
 	bargraph = getoutput(command)
 
 	print("Bar Graph:")
@@ -155,44 +223,72 @@ def get_bargraph_display():
 	bar = 'ON' if bargraph[1] == '*' else 'OFF'; print("    Bar2 is %s" %bar)
 	bar = 'ON' if bargraph[0] == '*' else 'OFF'; print("    Bar1 is %s" %bar)
 
-
-def set_bargraph_display(value):
-	'''Function to set bar-graph display.
-
-	'''
-	str_value = "%d" %(value)
-	command = 'echo' + ' ' + str_value + ' > ' + DEVICE_PATH[1] + '/device/bargraph ' + '2>/dev/null'
-	#(status, output) = getstatusoutput(command)
-	return status
+LIGHT_ONE_BAR = b'1'
+CLEAR_ONE_BAR = b'2'
+LIGHT_ALL_BARS = b'3'
+CLEAR_ALL_BARS = b'4'
+GET_BAR_GRAPH_LIGHT_STATE = b'5'
+GET_MOUSE_POSITION = b'6'
+GET_MOUSE_POSITION_AS_INTERRUPT_MESSAGE = b'7'
+GET_7_SEGEMENT_STATE = b'8'
+SET_7_SEGEMENT_STATE = b'9'
+RESET_DEVICE = b'a'
+REENUMERATE_DEVICE = b'b'
+GET_DEV_INFO = b'c'
 
 def play_with_device():
 	'''Function to play with the device.
 
 	'''
 	while ( True ):
-		print("\nUSBFX TEST -- Functions:\n\n");
-		print("\t1.  Light Bar\n");
-		print("\t2.  Clear Bar\n");
-		print("\t3.  Light entire Bar graph\n");
-		print("\t4.  Clear entire Bar graph\n");
-		print("\t5.  Get bar graph state\n");
-		print("\t6.  Get Mouse position\n");
-		print("\t7.  Get Mouse Interrupt Message\n");
-		print("\t8.  Get 7 segment state\n");
-		print("\t9.  Set 7 segment state\n");
-		print("\ta. Reset the device\n");
-		print("\tb. Reenumerate the device\n");
-		print("\n\t0. Exit\n");
-		print("\n\tSelection: ");
+		print("\nUSBFX TEST -- Functions:");
+		print("\t1.  Light Bar");
+		print("\t2.  Clear Bar");
+		print("\t3.  Light entire Bar graph");
+		print("\t4.  Clear entire Bar graph");
+		print("\t5.  Get bar graph state");
+		print("\t6.  Get Mouse position");
+		print("\t7.  Get Mouse Interrupt Message");
+		print("\t8.  Get 7 segment state");
+		print("\t9.  Set 7 segment state");
+		print("\ta. Reset the device");
+		print("\tb. Reenumerate the device");
+		print("");
+		print("\t0. Exit");
+		print("\tSelection: ");
 	        
 		try:
-			function = int(getch()) # TBD this getch can not accept char other than [0-9]
+			function = getch()
 		except:
 			print("Error reading input!");
 			break
 
-		print("select %d" %(function))
-		#if 
+		if function == LIGHT_ONE_BAR:
+			handle_light_one_bar()
+			
+		elif function == CLEAR_ONE_BAR:
+			handle_clear_one_bar()
+			
+		elif function == LIGHT_ALL_BARS:
+			handle_light_all_bars()
+			
+		elif function == CLEAR_ALL_BARS:
+			handle_clear_all_bars()
+
+		elif function == GET_BAR_GRAPH_LIGHT_STATE:
+			handle_get_bar_state()
+
+		elif function == GET_MOUSE_POSITION or \
+		     function == GET_MOUSE_POSITION_AS_INTERRUPT_MESSAGE or \
+		     function == GET_7_SEGEMENT_STATE or \
+		     function == SET_7_SEGEMENT_STATE or \
+		     function == RESET_DEVICE or \
+		     function == REENUMERATE_DEVICE:
+		     print("OSRFX2 -- to be done")
+		     
+		else:
+			print("Exiting ~~~")
+			sys.exit()
 		
 
 def main(argv):
@@ -202,14 +298,14 @@ def main(argv):
 	if False == parse_arg(argv):
 		sys.exit()
 
-	DEVICE_PATH = get_device_path()
-	if None == DEVICE_PATH:
+	_device_path = get_device_path()
+	if None == _device_path:
 		print("Can't find OSR USB-FX2 device")
 #		sys.exit()
 
 #	print("find OSR USB-FX2 device:")
-#	print(" - %s" %(DEVICE_PATH[0]))
-#	print(" - %s -> %s" %(DEVICE_PATH[1]))
+#	print(" - %s" %(_device_path[0]))
+#	print(" - %s -> %s" %(_device_path[1]))
 
 	# start process
 
